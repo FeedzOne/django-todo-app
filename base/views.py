a04f82b77419c829aa59c48f932df6a18a0a82a2
FeedzOne <thekubsonyt@gmail.com>
2023-01-04 20:19:29 -0800

    creating login, logout, task confirm, task form, task list, and task, starting to protect routes

--- base/views.py
+++ base/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Task
 
-class TaskList(ListView):
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self): 
+        return reverse_lazy('tasks')
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'Tasks'
 
@@ -11,3 +29,18 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'Task'
     template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'Task'
+    success_url = reverse_lazy('tasks')
