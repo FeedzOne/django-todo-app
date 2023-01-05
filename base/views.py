3db04bb20bca128479d8e2c7d00d82ca4dd02cf7
FeedzOne <thekubsonyt@gmail.com>
2023-01-04 23:06:45 -0800

    separating tasks between users

--- base/views.py
+++ base/views.py
@@ -22,25 +22,36 @@ class CustomLoginView(LoginView):
         return reverse_lazy('tasks')
 
 class TaskList(LoginRequiredMixin, ListView):
-    model = Task
+    model = Task 
     context_object_name = 'Tasks'
 
-class TaskDetail(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['Tasks'] = context['Tasks'].filter(user=self.request.user)
+        context['count'] = context['Tasks'].filter(complete=False).count()
+        return context
+        
+
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'Task'
     template_name = 'base/task.html'
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
-class TaskUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
-class DeleteView(DeleteView):
+class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'Task'
     success_url = reverse_lazy('tasks')
